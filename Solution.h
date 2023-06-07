9c6ec8ef041780ae9a88bef32debb06062b5b9ac
e7e26e5 <abc@xyz.com>
2023-06-07 23:40:23 +0800

    update

--- Solution.h
+++ Solution.h
@@ -34,41 +34,17 @@ struct ListNode {
 
 class Solution {
 public:
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        return mergeKLists(lists, 0, lists.size() - 1);
-    }
-
-private:
-    ListNode *mergeKLists(vector<ListNode *> &lists, int lo, int hi) {
-        if (lo > hi) {
-            return nullptr;
-        }
-        if (lo == hi) {
-            return lists[lo];
-        }
-        int mid = lo + (hi - lo) / 2;
-        ListNode *left = mergeKLists(lists, lo, mid);
-        ListNode *right = mergeKLists(lists, mid + 1, hi);
-        return mergeTwoLists(left, right);
-    }
-
-    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
-        auto *dummyHead = new ListNode();
-        ListNode *ptr = dummyHead;
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val < list2->val) {
-                ptr->next = list1;
-                list1 = list1->next;
-            } else {
-                ptr->next = list2;
-                list2 = list2->next;
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                return true;
             }
-            ptr = ptr->next;
         }
-        ptr->next = (list1 == nullptr ? list2 : list1);
-        ListNode *head = dummyHead->next;
-        delete dummyHead;
-        return head;
+        return false;
     }
 };
 
