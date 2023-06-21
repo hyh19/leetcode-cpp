8ef7e83436f58bac6bf4edb16baca9b66eb89dce
e7e26e5 <abc@xyz.com>
2023-06-21 09:37:40 +0800

    update

--- Solution.h
+++ Solution.h
@@ -1,11 +1,13 @@
 //
-// Created by HUANGYUHUI on 2023/2/20.
+// Created by HUANGYUHUI on 2023/6/21.
 //
 
 #ifndef LEETCODE_CPP_SOLUTION_H
 #define LEETCODE_CPP_SOLUTION_H
 
 #include <vector>
+#include <list>
+#include <unordered_map>
 
 using namespace std;
 
@@ -32,20 +34,154 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Solution {
+struct MyListNode {
+    int key, val;
+    MyListNode *prev;
+    MyListNode *next;
+
+    MyListNode(int k, int v) : key{k}, val{v}, prev{nullptr}, next{nullptr} {}
+};
+
+class MyDoublyLinkedList {
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode *slow = head;
-        ListNode *fast = head;
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-            if (slow == fast) {
-                return true;
+    MyDoublyLinkedList() : first{nullptr}, last{nullptr}, n{0} {}
+
+    ~MyDoublyLinkedList() {
+        while (!empty()) {
+            removeFirst();
+        }
+    }
+
+    void addLast(MyListNode *x) {
+        if (n == 0) {
+            first = x;
+            last = x;
+        } else {
+            last->next = x;
+            x->prev = last;
+            last = x;
+        }
+        ++n;
+    }
+
+    void removeFirst() {
+        MyListNode *x = first;
+        if (n == 1) {
+            first = nullptr;
+            last = nullptr;
+        } else {
+            first = first->next;
+            first->prev = nullptr;
+        }
+        delete x;
+        --n;
+    }
+
+    void removeLast() {
+        MyListNode *x = last;
+        if (n == 1) {
+            first = nullptr;
+            last = nullptr;
+        } else {
+            last = last->prev;
+            last->next = nullptr;
+        }
+        delete x;
+        --n;
+    }
+
+    void remove(MyListNode *x) {
+        if (x == first) {
+            removeFirst();
+        } else if (x == last) {
+            removeLast();
+        } else {
+            x->prev->next = x->next;
+            x->next->prev = x->prev;
+            delete x;
+            --n;
+        }
+    }
+
+    const MyListNode *getFirst() const {
+        return first;
+    }
+
+    bool empty() const {
+        return first == nullptr;
+    }
+
+private:
+    MyListNode *first;
+    MyListNode *last;
+    int n;
+};
+
+class LRUCache {
+public:
+    explicit LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+
+    int get(int key) {
+        if (contains(key)) {
+            return touchCache(key, INT_MIN);
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (capacity > 0) {
+            if (contains(key)) {
+                touchCache(key, value);
+            } else {
+                if (full()) {
+                    removeCache();
+                }
+                addCache(key, value);
             }
         }
-        return false;
     }
+
+private:
+    bool contains(int key) {
+        return keyToNode.count(key) == 1;
+    }
+
+    bool full() {
+        return keyToNode.size() == capacity;
+    }
+
+    void addCache(int key, int val) {
+        MyListNode *x = new MyListNode(key, val);
+        list.addLast(x);
+        keyToNode[key] = x;
+    }
+
+    void removeCache() {
+        int key = list.getFirst()->key;
+        list.removeFirst();
+        keyToNode.erase(key);
+    }
+
+    int touchCache(int key, int val) {
+        MyListNode *x = keyToNode[key];
+        int newVal = x->val;
+        if (val != INT_MIN) {
+            newVal = val;
+        }
+        list.remove(x);
+        x = new MyListNode(key, newVal);
+        list.addLast(x);
+        keyToNode[key] = x;
+        return newVal;
+    }
+
+private:
+    int capacity;
+    MyDoublyLinkedList list;
+    unordered_map<int, MyListNode *> keyToNode;
 };
+// https://leetcode.cn/submissions/detail/391594600/
 
 #endif //LEETCODE_CPP_SOLUTION_H
